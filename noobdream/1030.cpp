73fcb916735dbea086efbd742559f0545302fc42
xuzf-git <3349297129@qq.com>
2021-03-05 09:49:27 +0800

    3.5 push

---
+++ noobdream/1030.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int e, v;
+    while (cin >> e >> v)
+    {
+        cout << e - v + 2 << endl;
+    }
+    
+    return 0;
+}
